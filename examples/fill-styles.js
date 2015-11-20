eca48d640ee081ca8d1b763be0c884328815a44b
Andrey Sidorov <sidorares@yandex.com>
2015-11-20 12:34:17 +1100

    resolve keysym to unicode codepoint using substack/keysym

--- examples/fill-styles.js
+++ examples/fill-styles.js
@@ -11,19 +11,20 @@ ntk.createClient( (err, app) => {
 
   wnd.on('expose', (ev) => {
     ctx.fillRect(ev.x, ev.y, ev.width, ev.height, 1, 0.5, 0.5, 1);
-    ctx.font = '42pt TimesNewRoman';
+    ctx.font = '42pt DroidSansHebrew';
     var w = ctx.measureText(text).width;
     ctx.fillStyle = ctx.createLinearGradient(0, 0, w, 0)
       .addColorStop(0, 'red')
       .addColorStop(1, 'blue');
-    ctx.fillText(text, 50, 250);
+    ctx.fillText(text, 750 - w, 250);
   });
   var shift = 0;
   wnd.on('keydown', function(ev) {
-    if (ev.mappedKey == 65288)
+    if (ev.codepoint == 8)
       text = text.slice(0, -1);
-    else if (ev.mappedKey < 255)
-      text = text + String.fromCharCode(ev.mappedKey);
+    else if (ev.codepoint)
+      text = text + String.fromCodePoint(ev.codepoint);
+    
     wnd.emit('expose', { x: 0, y: 0, width: 1000, height: 1000});
   });
   wnd.on('mousemove', function(ev) {
