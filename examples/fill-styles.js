35d2e826fe5601584aa0d22e9f7c5fb2830cf19e
Andrey Sidorov <sidorares@yandex.com>
2018-04-21 14:43:45 +1000

    example

--- examples/fill-styles.js
+++ examples/fill-styles.js
@@ -4,7 +4,7 @@ ntk.createClient( (err, app) => {
   var wnd = app.createWindow({ width: 800, height: 600});
   var ctx = wnd.getContext('2d');
 
-  var text = 'This-is-Test\u2038 test';
+  var text = 'This is Test test';
 
   var lastx = 0;
   var lasty = 0;
@@ -12,13 +12,13 @@ ntk.createClient( (err, app) => {
   wnd.on('expose', (ev) => {
     ctx.fillStyle = 'white';
     ctx.fillRect(ev.x, ev.y, ev.width, ev.height, 1, 0.5, 0.5, 1);
-    ctx.font = "190px 'Times New Roman'";
+    ctx.font = "50px 'Times New Roman'";
     var w = ctx.measureText(text).width;
     ctx.fillStyle = ctx.createLinearGradient(0, 0, w, 100)
       .addColorStop(0, 'red')
-      .addColorStop(1, 'blue'); 
-    ctx.fillStyle = 'rgba(200, 200, 180, 0.7)';
-    ctx.fillText(text, 750 - w, 250);
+      .addColorStop(1, 'blue');
+    ctx.fillStyle = 'red'; //'rgba(200, 200, 180, 0.7)';
+    ctx.fillText(text, 100, 100);// 750 - w, 250);
   });
   var shift = 0;
   wnd.on('keydown', function(ev) {
